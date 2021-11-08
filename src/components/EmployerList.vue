120adad54fdf7304ae8106556fa32cc04d77df28
Artem <asustransformertf300x@gmail.com>
2021-11-08 17:52:58 +0300

    Created a styles for records

--- src/components/EmployerList.vue
+++ src/components/EmployerList.vue
@@ -25,18 +25,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.list{
+  text-align: start;
+  display: flex;
+  justify-content: center;
+  text-justify: 5px;
 }
 ul {
   list-style-type: none;
   padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  line-height: 1.5rem;
+  padding: 15px;
+  border: 1px solid #cacaca;
+  margin: 5px;
 }
 </style>
