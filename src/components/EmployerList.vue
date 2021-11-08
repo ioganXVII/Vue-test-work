e8399e30929c83408ebef97fc9320ce9dd0a98ea
Artem <asustransformertf300x@gmail.com>
2021-11-09 01:48:45 +0300

    Created a List element component

--- src/components/EmployerList.vue
+++ src/components/EmployerList.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="list">
-    <ul>
-      <li class="employer" v-for="employer in employers" :key = "employer.id">
-          First Name: {{employer.first_name}} <br>
-          Last Name: {{employer.last_name}} <br>
-          Email: {{employer.email}} <br>
-          Gender: {{employer.gender}} <br>
-      </li>
+    <ul v-for="employer in employers" :key="employer.id">
+          <ListElement :employer="employer"/>
     </ul>
   </div>
 </template>
 
 <script>
+import ListElement from './ListElement.vue'
+
 export default {
   name: 'EmployerList',
+  components: {
+    ListElement,
+  },
   props: {
     employers: {
       type: Array,
@@ -25,20 +25,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.list{
+ul {
   text-align: start;
   display: flex;
   justify-content: center;
   text-justify: 5px;
-}
-ul {
   list-style-type: none;
-  padding: 0;
 }
-li {
-  line-height: 1.5rem;
-  padding: 15px;
-  border: 1px solid #cacaca;
-  margin: 5px;
-}
+
 </style>
---
+++ src/components/ListElement.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+      First Name: {{ employer.first_name }} <br>
+      Last Name: {{ employer.last_name }} <br>
+      E-mail: {{ employer.email }} <br>
+      Gender: {{ employer.gender }} <br>
+  </li>
+</template>
+
+<script>
+export default {
+    name: 'ListElement',
+    props: {
+        employer:{
+            type: Array,
+            required: true,
+        }
+    },
+}
+</script>
+
+<style>
+li {
+  width: 250px;
+  line-height: 1.5rem;
+  padding: 15px;
+  border: 1px solid #cacaca;
+  margin: 5px;
+}
+</style>
